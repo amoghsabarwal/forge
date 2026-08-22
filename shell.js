899ca0c512f26dd5f27a5e85c072c3bd763ca8a9
Amogh Sabarwal <amoghsabarwal@gmail.com>
2026-08-23 04:29:34 +0530

    Delete shell.js

--- shell.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  'use strict';
-  // Forge has one editor surface. Layouts, layers, effects and interaction live inside it.
-  window.FORGE_MODE = 'editor';
-  // Compatibility hook used by the layer renderer after media updates.
-  window.syncCanvas = window.syncCanvas || function(){};
-})();
